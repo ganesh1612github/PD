392fd44cce5d6ab1858f1c6032524ece09ec65b0
Ganesh Chavan <ganeshsureshchavan16@gmail.com>
2023-01-21 14:49:32 +0530

    CR-122 changes

--- demo.java
+++ demo.java
@@ -4,5 +4,12 @@ public class Demo{
 
     public static void main (String[]args){
           System.out.println("Hi.........."); 
-    }
+
+          
+   }
+          //CR-122
+         public void doProcess(){
+          //logic
+          }
+    
 }
